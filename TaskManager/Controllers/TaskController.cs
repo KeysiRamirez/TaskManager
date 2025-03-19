17b1e7930887eca86b55c9152833305fda49463f
KEYSI MICHELL RAMIREZ MATEO <k.ramirezm@unapec.edu.do>
2025-03-18 21:59:21 -0400

    Updates

--- TaskManager/Controllers/TaskController.cs
+++ TaskManager/Controllers/TaskController.cs
@@ -59,9 +59,9 @@ namespace TaskManager.API.Controllers
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        [Authorize]
+
         [HttpGet("GetTask")]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get() 
         {
             OperationResult<List<TaskModel<string>>> result = await _task.GetAll();
             if (!result.Success) return BadRequest(result);
@@ -98,5 +98,77 @@ namespace TaskManager.API.Controllers
            
             return Ok(result);
         }
+
+        [HttpPut("UpdateTask")]
+        public async Task<IActionResult> Put([FromBody] TaskEntity<string> entity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (entity.TaskId <= 0)
+            {
+                return BadRequest("El ID de la tarea es inválido.");
+            }
+
+            if (entity.DueDate <= DateTime.Now)
+            {
+                return BadRequest("La fecha de vencimiento no puede ser antes del día actual.");
+            }
+
+            if (string.IsNullOrEmpty(entity.TaskDescription))
+            {
+                return BadRequest("La descripción no puede estar vacía.");
+            }
+
+            TaskEntity<string> newTask = new TaskEntity<string>()
+            {
+                TaskId = entity.TaskId,  
+                TaskDescription = entity.TaskDescription,
+                DueDate = entity.DueDate,
+                TaskStatus = entity.TaskStatus,
+                AdditionalData = entity.AdditionalData
+            };
+
+            var result = await _task.Update(newTask);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpDelete("DeleteTask")]
+        public async Task<IActionResult> Delete([FromBody] TaskEntity<string> entity)
+        {
+            OperationResult<TaskModel<string>> result = null;
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            TaskEntity<string> newTask = new TaskEntity<string>()
+            {
+                TaskId = entity.TaskId,
+                TaskDescription = entity.TaskDescription,
+                DueDate = entity.DueDate,
+                TaskStatus = entity.TaskStatus,
+                AdditionalData = entity.AdditionalData
+            };
+
+            result = await _task.Remove(newTask);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
     }
+
 }
--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TaskManager.API.Hubs;
 using TaskManager.Data.Context;
 using TaskManager.Data.Interfaces;
@@ -22,6 +25,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 app.UseRouting();
 
@@ -34,6 +55,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
